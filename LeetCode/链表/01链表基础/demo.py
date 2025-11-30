6ee0bedbfbaea6387860f0feb515980a3bd15abb
zhouyihe <1552951165@qq.com>
2025-11-30 23:32:29 +0800

    feat(list): 添加链表基础练习代码
    - 新增链表节点定义和反转链表的框架代码
    - 添加成绩等级判断的示例代码
    - 初始化链表相关练习文件结构

---
+++ LeetCode/链表/01链表基础/demo.py
@@ -0,0 +1,6 @@
+score=float(input("你这次考试考了多少分？"))
+print('厉害！') if score >= 90 else \
+print('优秀！') if score >= 80 else \
+print('良好！') if score >= 70 else \
+print('及格！') if score >= 60 else \
+print('不及格！')
