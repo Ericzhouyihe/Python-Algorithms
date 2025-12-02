ac19661f87f736f7fe520bb327a8def7e4490df8
zhouyihe <1552951165@qq.com>
2025-12-02 21:38:38 +0800

    feat(list): 添加链表基础练习代码
    - 新增链表节点定义和反转链表的框架代码
    - 添加成绩等级判断的示例代码
    - 初始化链表相关练习文件结构

--- LeetCode/demo.py
+++ LeetCode/demo.py
@@ -1,7 +1,16 @@
-def func(a, b, c):
-    print(a, b, c)
+def outer():
+    global a, b
+    a, b, c, d = 3, 4, 5, 6
+    print(a, b)
 
 
-tup = (1, 2, 3)
-d = {'a': 1, 'b': 2, 'c': 3}
-func(**d)
+    def inner():
+        global a, b
+        nonlocal c, d
+        a, b, c, d = 7, 8, 9, 0
+    inner()
+    print(c, d)
+
+a, b = 1, 2
+outer()
+print(a, b)
