2680a4f0cd7c8ab65532adae32a2caa03f873506
zhouyihe <1552951165@qq.com>
2025-12-05 00:00:54 +0800

    feat(leetcode): 添加链表基础题目实现
    - 实现了复制带随机指针的链表（t138.py）
    - 实现了回文链表判断（t234.py）
    - 添加了必要的类型注解和导入模块
    - 提供了基本的链表节点定义和解决方案类

---
+++ LeetCode/链表/01链表基础/t234.py
@@ -0,0 +1,13 @@
+from typing import Optional
+
+from LeetCode.链表.ListNode import ListNode
+
+
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        vals = []
+        current_node = head
+        while current_node is not None:
+            vals.append(current_node.val)
+            current_node = current_node.next
+        return vals == vals[::-1]
