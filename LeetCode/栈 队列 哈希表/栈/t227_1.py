9c040960446c373fa8c55d72a0708f2122ee183e
Administrator <1552951165@qq.com>
2025-12-23 17:03:01 +0800

    feat(algorithm): 添加基本计算器II算法实现
    - 实现字符串表达式计算功能
    - 支持加减乘除四则运算
    - 整数除法仅保留整数部分
    - 使用栈数据结构处理运算符优先级
    - 正确处理空格字符跳过
    - 实现数字字符到整数的转换逻辑

---
+++ LeetCode/栈 队列 哈希表/栈/t227_1.py
@@ -0,0 +1,27 @@
+"""
+基本计算器II
+给你一个字符串表达式 s ，请你实现一个基本计算器来计算并返回它的值。
+整数除法仅保留整数部分。
+"""
+
+
+class Solution:
+    def calculate(self, s: str) -> int:
+        n = len(s)
+        stack = []
+        perSign = '+'
+        num = 0
+        for i in range(n):
+            if s[i] != ' ' and s[i].isdigit():
+                num = num * 10 + ord(s[i]) - ord('0')
+            if i == n - 1 or s[i] in '+-*/':
+                stack.append(num)
+            elif perSign == '-':
+                stack.append(-num)
+            elif perSign == '*':
+                stack.append(stack.pop() * num)
+            else:
+                stack.append(int(stack.pop() / num))
+            perSign = s[i]
+            num = 0
+        return sum(stack)
