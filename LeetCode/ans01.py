fee127304f2fb4e7975f9006466ce839ea750f57
zhouyihe <1552951165@qq.com>
2026-03-18 17:15:00 +0800

    斐波拉契的运行时间

---
+++ LeetCode/ans01.py
@@ -0,0 +1,28 @@
+import time
+
+def fibo(i):
+    if i == 0:
+        return 0
+    elif i == 1:
+        return 1
+    else:
+        return fibo(i - 1) + fibo(i - 2)
+
+# 记录开始时间
+start = time.time()
+
+count = 1
+
+while count < 1000000:
+    fibo(20)
+    count += 1
+
+print(fibo(20))
+
+# 记录结束时间
+end = time.time()
+duration_ms = end - start
+
+# 1277秒
+print(f"程序运行时间：{duration_ms} 秒")
+
