2840b78af1e04f8a204fbfd793e663b358a3d26b
zhouyihe <1552951165@qq.com>
2026-03-29 23:56:33 +0800

    refactor(stack): 优化字符串解码算法实现
    - 重构了数字解析逻辑，支持多位数处理
    - 简化了字符串重复操作的实现方式
    - 改进了栈中元素的处理流程
    - 添加了多个测试用例验证功能正确性
    - 优化了代码结构提升可读性

--- LeetCode/栈 队列 哈希表/栈/t394_f.py
+++ LeetCode/栈 队列 哈希表/栈/t394_f.py
@@ -1,6 +1,7 @@
 class Solution:
     def decodeString(self, s: str) -> str:
         stack = []
+        num = 0
         for c in s:
             if c == ']':
                 temp = []
@@ -10,14 +11,22 @@ class Solution:
                         temp.append(n)
                     else:
                         break
-                num = int(stack.pop())
-                for i in range(num):
-                    for j in range(len(temp)-1, -1, -1):
-                        stack.append(temp[j])
+                for i in range(int(stack.pop())):
+                    stack.append(''.join(temp)[::-1])
             else:
-                stack.append(c)
+                if c.isdigit():
+                    num = num * 10 + int(c)
+                else:
+                    if num != 0:
+                        stack.append(num)
+                        num = 0
+                    stack.append(c)
+
         return ''.join(stack)
 
 
 s = Solution()
+print(s.decodeString("3[z]2[2[y]pq4[2[jk]e1[f]]]ef"))
+print(s.decodeString("2[abc]3[cd]ef"))
 print(s.decodeString("100[leetcode]"))
+print(s.decodeString("3[a2[c]]"))
