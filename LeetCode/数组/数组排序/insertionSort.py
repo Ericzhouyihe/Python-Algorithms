92b0a23b48298889ba7c76f904510b419bb36f0a
一人一口粥 <1552951165@qq.com>
2026-04-07 09:30:46 +0800

    ``` feat(sorting): 实现数组插入排序算法
    - 添加插入排序核心逻辑实现
    - 使用原地排序方式优化空间复杂度
    - 实现元素逐个比较和插入过程
    - 返回排序后的新数组
    ```

--- LeetCode/数组/数组排序/insertionSort.py
+++ LeetCode/数组/数组排序/insertionSort.py
@@ -1,7 +1,13 @@
 class Solution:
     def insertionSort(self, nums: list[int]) -> list[int]:
         """插入排序算法实现"""
-
+        for i in range(1, len(nums)):
+            num = nums[i]
+            j = i
+            while j > 0 and nums[j - 1] > num:
+                nums[j] = nums[j - 1]
+                j -= 1
+            nums[j] = num
 
         return nums
 
