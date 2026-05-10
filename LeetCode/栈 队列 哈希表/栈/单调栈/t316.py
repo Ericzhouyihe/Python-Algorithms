3c60721ea382dfd4ab83c320e592fa248fd92cc0
zhouyihe <1552951165@qq.com>
2026-05-11 06:11:15 +0800

    去除重复字母

---
+++ LeetCode/栈 队列 哈希表/栈/单调栈/t316.py
@@ -0,0 +1,29 @@
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        last = {}
+
+        for i, ch in enumerate(s):
+            last[ch] = i
+
+        stack = []
+        visited = set()
+
+        for i, ch in enumerate(s):
+            if ch in visited:
+                continue
+
+            while stack and stack[-1] > ch and last[stack[-1]] > i:
+                removed = stack.pop()
+                visited.remove(removed)
+
+            stack.append(ch)
+            visited.add(ch)
+
+        return "".join(stack)
+
+
+# 测试
+if __name__ == "__main__":
+    s = Solution()
+    print(s.removeDuplicateLetters("bcabc"))
+    print(s.removeDuplicateLetters("cbacdcbc"))
