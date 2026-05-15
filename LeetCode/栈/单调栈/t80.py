86fa38051c8a5c0173998db4e84e4e98dbcf9000
一人一口粥 <1552951165@qq.com>
2026-05-15 17:32:39 +0800

    11

---
+++ LeetCode/栈/单调栈/t80.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        res = 0
+        stack = []
+
+        heights = [0] + heights + [0]
+        for i, h in enumerate(heights):
+            while stack and heights[stack[-1]] > h:
+                heights = heights[stack.pop()]
+                width = i - stack[-1] - 1
+                res = max(res, heights * width)
+            stack.append(i)
+
+        return res
+
+
+# 测试
+if __name__ == "__main__":
+    s = Solution()
+    print(s.largestRectangleArea([2, 1, 5, 6, 2, 3]))
+    print(s.largestRectangleArea([2, 4]))
+    print(s.largestRectangleArea([1, 1]))
+    print(s.largestRectangleArea([2, 1, 2]))
