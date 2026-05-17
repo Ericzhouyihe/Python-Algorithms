f2877fe9e5ed810f60fdbdf76edeec7f369a673e
zhouyihe <1552951165@qq.com>
2026-05-17 22:35:10 +0800

    316. 去除重复字母

--- LeetCode/栈/单调栈/t316.py
+++ LeetCode/栈/单调栈/t316.py
@@ -22,6 +22,13 @@ class Solution:
         return "".join(stack)
 
     def removeDuplicateLetters1(self, s: str) -> str:
+        """
+        要让结果字典序最小，就希望前面的字符尽量小
+        但又不能随便删，因为每个字母最后必须保留一次，而且不能打乱相对顺序
+        所以当你遍历字符串时，遇到一个字符 c，如果栈顶字符比 c 大，并且栈顶字符后面还会再出现，那就可以把栈顶弹掉，让更小的 c 往前排
+        判断“后面还会不会出现”，通常用每个字符最后一次出现的位置
+        """
+
         last = {ch: i for i, ch in enumerate(s)}
         stack = []
         used = set()
