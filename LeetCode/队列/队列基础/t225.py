550fd21f46826c550aad765b79e58628e67b0fa0
zhouyihe <1552951165@qq.com>
2026-05-30 08:57:18 +0800

    225. 用队列实现栈 两个队列 和 一个队列 两种实现

--- LeetCode/队列/队列基础/t225.py
+++ LeetCode/队列/队列基础/t225.py
@@ -64,17 +64,15 @@ class MyStack2:
         self.deque = deque()
 
     def push(self, x: int) -> None:
-        self.q2.append(x)
-        while self.q1:
-            self.q2.append(self.q1.popleft())
-
-        self.q1, self.q2 = self.q2, self.q1
+        self.deque.append(x)
+        for _ in range(len(self.deque) - 1):
+            self.deque.append(self.deque.popleft())
 
     def pop(self) -> int:
-        return self.q1.popleft()
+        return self.deque.popleft()
 
     def top(self) -> int:
-        return self.q1[0]
+        return self.deque[0]
 
     def empty(self) -> bool:
-        return not self.q1
+        return not self.deque
