b052378d51806af4e00d111ce7c5dddd8a643190
zhouyihe <1552951165@qq.com>
2026-05-31 23:25:09 +0800

    215. 数组中的第K个最大元素

--- LeetCode/队列/优先队列/t215.py
+++ LeetCode/队列/优先队列/t215.py
@@ -21,19 +21,11 @@ class Solution:
             elif num > res[0]:
                 heapq.heappop(res)
                 heapq.heappush(res, num)
-                
+
         return heapq.heappop(res)
 
 
 # 测试
 if __name__ == "__main__":
     s = Solution()
-    print(s.findKthLargest([3, 2, 1, 5, 6, 4], 2))
-    print(s.findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4))
-    print(s.findKthLargest([3, 2, 1, 5, 6, 4], 1))
-    print(s.findKthLargest([1], 1))
-    print(s.findKthLargest([1, 2], 2))
-    print(s.findKthLargest([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5))
-    print(s.findKthLargest([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10))
-    print(s.findKthLargest([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1))
-    print(s.findKthLargest([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0))
+    print(s.findKthLargest1([3, 2, 1, 5, 6, 4], 2))
