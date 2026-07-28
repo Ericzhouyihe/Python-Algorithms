c0d2279757fd3857dc8aee0634bd440ef78438e3
zhouyihe <1552951165@qq.com>
2026-07-28 23:12:49 +0800

    新增二叉树后序遍历实现，支持递归和非递归方式

--- LeetCode/树/二叉树/TreeNode.py
+++ LeetCode/树/二叉树/TreeNode.py
@@ -97,16 +97,54 @@ class Solution:
 
         resturn = res
 
-    def postorderTraversal(self, root:TreeNode) -> List[int]:
+    def postorderTraversal01(self, root:TreeNode) -> List[int]:
         """
         二叉树后序遍历(递归)
         :param root: 根节点
         :return: 后序遍历结果
         """
+        res = []
+
+        def postorder(node: TreeNode):
+            if not node:
+                return
+            if node.right:
+                postorder(node.right)
+            if node.left:
+                postorder(node.left)
+            res.append(node.val)
+
+        postorder(root)
+        return res
+
+    def postorderTraversal02(self, root: TreeNode) -> List[int]:
+        """
+        二叉树后序遍历(非递归)
+        :param root: 根节点
+        :return: 后序遍历结果
+        """
+        res = []
         stack = []
+        prev = None
 
-        def inorder(node):
-            while stack:
-                node = stack.pop()
-                if node.right:
+        while root or stack:  # 只要当前节点不为空或栈不为空就继续遍历
+            # 一直向左走，将所有左子节点入栈
+            while root:
+                stack.append(root)      # 当前节点入栈
+                root = root.left        # 继续遍历左子树
 
+            node = stack.pop()          # 弹出栈顶节点，准备访问或遍历其右子树
+
+            # 判断是否可以访问当前节点
+            # 1. 没有右子树
+            # 2. 右子树已经访问过（即上一次访问的节点是当前节点的右子节点）
+            if not node.right or node.right == prev:
+                res.append(node.val)    # 访问当前节点
+                prev = node             # 更新上一次访问的节点
+                root = None             # 当前节点已访问，重置root，防止重复入栈
+            else:
+                # 右子树还未访问，当前节点重新入栈，转而遍历右子树
+                stack.append(node)
+                root = node.right
+
+        return res
