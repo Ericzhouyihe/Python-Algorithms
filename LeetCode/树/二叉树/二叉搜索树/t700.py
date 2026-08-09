f8a3d3cfac569ac7025e19989076d2ce219a15aa
zhouyihe <1552951165@qq.com>
2026-08-09 10:43:18 +0800

    t700

---
+++ LeetCode/树/二叉树/二叉搜索树/t700.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+from typing import Optional
+
+
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            return
+        if val < root.val:
+            return self.searchBST(root.left, val)
+        elif val > root.val:
+            return self.searchBST(root.right, val)
+        return root
+
+
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        if root is None:
+            return None
+        if val == root.val:
+            return root
+        return self.searchBST(root.left if val < root.val else root.right, val)
