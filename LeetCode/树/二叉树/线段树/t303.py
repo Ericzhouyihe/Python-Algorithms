49ca382b387f2081db543ce76386cb595d0d6a04
zhouyihe <1552951165@qq.com>
2026-08-14 20:12:43 +0800

    新增NumArray类及其sumRange方法实现

---
+++ LeetCode/树/二叉树/线段树/t303.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.sums = [0]
+        _sums = self.sums
+
+        for num in nums:
+            _sums.append(_sums[-1] + num)
+
+    def sumRange(self, left: int, right: int) -> int:
+        _sum = self.sums
+        return _sum[right + 1] - _sum[left]
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
