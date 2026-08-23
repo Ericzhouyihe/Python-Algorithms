afb4b0ac0ccebc06cfaad064cfe104fc46f0fd21
zhouyihe <1552951165@qq.com>
2026-08-23 13:10:00 +0800

    新增线段树和树状数组的实现，包含构建、更新和查询功能

---
+++ LeetCode/树/树状数组/BinaryIndexedTree.py
@@ -0,0 +1,23 @@
+class BinaryIndexedTree:
+    def __init__(self, n):
+        self.n = n
+        self.tree = [0] * (n + 1)
+
+    def lowbit(self, x):
+        return x & (-x)
+
+    def build(self, arr):
+        for i in range(len(arr)):
+            self.update(i + 1, arr[i])
+
+    def update(self, index, val):
+        while index <= self.n:
+            self.tree[index] += val
+            index += self.lowbit(index)
+
+    def query(self, index):
+        res = 0
+        while index > 0:
+            res += self.tree[index]
+            index -= self.lowbit(index)
+        return res
